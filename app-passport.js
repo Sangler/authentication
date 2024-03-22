26385d13ccfe9cc441d93b7b6acc8844bae0ddf4
Sangler <134127453+Sangler@users.noreply.github.com>
2024-03-22 17:07:48 -0400

    Update app-passport.js
    Google + facebook authentication

--- app-passport.js
+++ app-passport.js
@@ -5,25 +5,26 @@ const mongoose = require('mongoose');
 const session  = require('express-session');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const flash = require('connect-flash');
 
-//const crypto = require('crypto');
 const passport = require('passport');
 
 //const LocalStrategy = require('passport-local');
 //const User = require('./models/index.js'); //mongoose schema
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-const UserOAUTH = require('./models/oauth.js'); //mongoose schema
+const GgUserOAUTH = require('./models/oauthGoogle.js'); //Google mongoose schema
+const FBUserOAUTH = require('./models/oauthFacebook.js'); //Facebook mongoose schema
 
 const router = require('./routers/user.js');
-const PORT = 3000;
+const PORT = 7070
 const keys = require('./utils/process-env.js');
-const oauth = require("./models/oauth.js");
+const testRouter = require("./routers/player.js");
 
-//Mongoose connection & Must have
+
+//Mongoose connection & set-up
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/testdb')
+mongoose.connect('mongodb://127.0.0.1:27017/carddb')
 .then(data=>{
   console.log('Attach schema')
 })
@@ -49,7 +50,7 @@ const sessionConfig = {
   saveUninitialized: false,
   cookie: {
       httpOnly: true,
-      expires: Date.now() + 1000 * 60 * 60,// * 24 * 7,
+      expires: Date.now() + 1000 * 60* 60,// * 24 * 7,
       maxAge: 1000 * 60 * 60//* 60 * 24 * 7
   }
 }
@@ -69,18 +70,19 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret,
-  callbackURL: '/google/redirect',
+  callbackURL: '/login/google/redirect',
   scope: ['email','profile']
 }, async function verify(accessToken, refreshToken, profile, done){
   try {
-    console.log(`Email: ${profile.emails[0].value}`);
-    const existingUser = await UserOAUTH.findOne({ google_Id: profile.id });
+    console.log(`Email: ${profile.emails}`);
+    const existingUser = await GgUserOAUTH.findOne({ google_Id: profile.id });
 
     if (existingUser) {
         done(null, existingUser);
         
     } else {
-        const new_oauth_user = new UserOAUTH({
+        const new_oauth_user = new GgUserOAUTH({
+
             user_email: profile.emails[0].value.toString(),
             username: profile.displayName,
             google_Id: profile.id,
@@ -98,25 +100,90 @@ passport.use(new GoogleStrategy({
 }
 }));
 
+
+
+passport.use(new FacebookStrategy({
+  clientID: keys.facebook.App_ID,
+  clientSecret: keys.facebook.App_secret,
+  callbackURL: '/login/facebook/callback', // The redirect URL after successful authentication
+  profileFields: ['id', 'emails', 'displayName', 'photos', 'locale'] // Specify the fields you need
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+          // Check if the user exists in your database
+    const existingUser = await FBUserOAUTH.findOne({ facebook_Id: profile.id });
+
+    if (existingUser) {
+      // User already exists, update the last login date and return the user
+      existingUser.lastLoginAt = new Date();
+      await existingUser.save();
+      done(null, existingUser);
+    } else {
+      // Create a new user if not exists
+      const new_oauth_user = new FBUserOAUTH({
+        username: profile.displayName,
+        FB_ID: profile.id,
+        picture: profile.photos ? profile.photos[0].value : null,
+        location: profile._json.locale,
+        createdAt: new Date(),
+        lastLoginAt: new Date() // Set last login date for a new user
+      });
+
+      await new_oauth_user.save();
+      done(null, new_oauth_user); // Return the new user
+    }
+
+  } catch (error) {
+      console.error('Facebook OAuth Error:', error);
+      done(error); // Pass the error to done
+  }
+}));
+
+
+
 passport.serializeUser((user, done) => {
+  //this comes from req.session.passport.user (ObjectID in mongodb)
   done(null, user.id);
 });
 
-passport.deserializeUser( (id, done) => {
-  UserOAUTH.findById(id).then((user)=>{done(null, id);})
+passport.deserializeUser( async(id, done) => {
+  try {
+    const userGoogle = await GgUserOAUTH.findById(id);
+    const userFacebook = await FBUserOAUTH.findById(id);
+  //check if valid object (req.session.passport.user)
+
+    if (userGoogle) {
+      done(null, userGoogle);
+    } else if (userFacebook) {
+      done(null, userFacebook);
+    } else {
+      done(new Error('User not found in either schema'));
+    }
+  } catch (error) {
+    console.error('Error deserializing user:', error);
+    done(error);
+  }
 });
 
-//middleware
-
+//Middlewares
 //Running router requests 
 app.use('/', router);
+app.use('/test/', testRouter);
 
 //Catch all not found pages
 app.get("*", (req,res) => {
   res.status(404).render("error.ejs") 
 });
+/*
+
+*/
+const server = http.createServer((req,res)=>{
+  res.end("connected!")
+
+});
+// server.listen(PORT)
 
 //RUN THE SERVER
 app.listen(PORT, () => {
+  
   console.log(`http://localhost:${PORT}`)
 });
