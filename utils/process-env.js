24d696c6daec78b2913ce627b201cdae0186ca31
Sangler <134127453+Sangler@users.noreply.github.com>
2024-09-19 14:08:13 -0400

    Update process-env.js

--- utils/process-env.js
+++ utils/process-env.js
@@ -4,12 +4,12 @@ module.exports= {
 		secret: 'this-should-be-a-better-secret!'
 	},
 	google:{
-		clientID: '599647164505-g13akh88u51h49iaf3ufsk1f1ni2r7sj.apps.googleusercontent.com', // get from google
-		clientSecret: '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' // get from google
+		clientID: 'id', // get from google
+		clientSecret: 'secret' // get from google
 	},
 	facebook:{
-		App_ID:"788540709810086", //get from meta facebook
-		App_secret:'90d6df9ca9c16c43cd85ffca25fd5f80'
+		App_ID:"id", //get from meta facebook
+		App_secret:'secret'
 	}
 }
 	
